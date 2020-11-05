f34d1ef7c9cd3cae15861b3442a7c75111262514
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-11-05 18:18:58 +0300

    Add post method to router

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Poll App' });
 });
 
+router.post('/', (req, res, next) => {
+  console.log(req.body);
+});
+
 module.exports = router;
