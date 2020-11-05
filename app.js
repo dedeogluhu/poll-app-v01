6b60745876dafa968132cb2131d4ed4ac74f46b3
Hüseyin Dedeoğlu <64230499+dedeogluhu@users.noreply.github.com>
2020-11-05 18:17:20 +0300

    Add cors to the project

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 
@@ -13,6 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
